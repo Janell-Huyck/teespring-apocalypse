98534f87bdcec889266289613701d73e2fbe99a4
Josh G <joshg@Teeadmin10160s-MacBook-Pro.local>
2020-10-16 17:37:24 -0400

    recieved update

--- apocalypse/src/components/dumpster.js
+++ apocalypse/src/components/dumpster.js
@@ -3,7 +3,7 @@ import CreateIncident from '../pages/create-incident'
 const Dumpster =(props) => {
     return (
         <div>
-            it's friday {props}
+            it's friday {props.incindent.incident}
         </div>
     )
 }
